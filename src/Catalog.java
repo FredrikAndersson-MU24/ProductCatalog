d6d34daa5273370fb715bd0fc7a40e35a33196d9
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2024-11-07 14:39:13 +0100

    updated Catalog and Meny

--- src/Catalog.java
+++ src/Catalog.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Catalog {
-
+Scanner scanner = new Scanner(System.in);
     ArrayList<Product> products = new ArrayList<>();
 
     public void generateCatalog() {
@@ -9,10 +10,20 @@ public class Catalog {
       products.add(new Product("apple", 2.5));
       products.add(new Product("banana", 3.0));
 
-        for(Product product : products){
-            System.out.println(product);
+        }
+        public void showProduct(){
+            for(Product product : products){
+                System.out.println(product);
+            }
         }
 
+        public void addProduct(Scanner scanner){
+            System.out.println("Enter product name: ");
+            String name = scanner.nextLine();
+            System.out.println("Enter product price: ");
+            double price = scanner.nextDouble();
+            Product product = new Product (name,price);
         }
+
 
 }
