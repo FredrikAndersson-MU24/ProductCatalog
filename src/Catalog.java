b46f40b09a3b570b53c81022b1e76b4cf0f43313
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2024-11-07 11:02:43 +0100

    second commit for testing

--- src/Catalog.java
+++ src/Catalog.java
@@ -2,5 +2,19 @@ import java.util.ArrayList;
 
 public class Catalog {
 
-    ArrayList<>
+    ArrayList<Catalog> catalogList = new ArrayList<>();
+
+    public void generateCatalog() {
+
+  catalogList.add(new Catalog("apple", 2.5));
+  catalogList.add(new Catalog("banana", 3.0));
+
+    for(Catalog catalog : catalogList){
+        System.out.println(catalog);
+    }
+            
+
+    }
+
+
 }
