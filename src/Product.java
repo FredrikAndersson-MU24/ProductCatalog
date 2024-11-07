bfd9e519b6cac93bf7c00c808dc99ecbccb7ca34
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2024-11-07 11:01:37 +0100

    added Product class

--- src/Product.java
+++ src/Product.java
@@ -10,4 +10,19 @@ public class Product {
         this.price = price;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
